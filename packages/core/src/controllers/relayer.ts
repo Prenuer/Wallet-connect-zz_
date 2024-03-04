4db8faea6be576b3edb831343457ce62a1a2ee09
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 15:59:27 +0100

    Initialize chore/log-client-id-again

---
+++ packages/core/src/controllers/relayer.ts
@@ -0,0 +1,219 @@
+import pino, { Logger } from "pino";
+import { EventEmitter } from "events";
+import { JsonRpcProvider } from "@walletconnect/jsonrpc-provider";
+import {
+  formatJsonRpcResult,
+  IJsonRpcProvider,
+  isJsonRpcRequest,
+  JsonRpcPayload,
+  JsonRpcRequest,
+} from "@walletconnect/jsonrpc-utils";
+import WsConnection from "@walletconnect/jsonrpc-ws-connection";
+import {
+  generateChildLogger,
+  getDefaultLoggerOptions,
+  getLoggerContext,
+} from "@walletconnect/logger";
+import { RelayJsonRpc } from "@walletconnect/relay-api";
+import { toMiliseconds } from "@walletconnect/time";
+import {
+  ICore,
+  IMessageTracker,
+  IPublisher,
+  IRelayer,
+  ISubscriber,
+  RelayerOptions,
+  RelayerTypes,
+} from "@walletconnect/types";
+import { formatRelayRpcUrl, getInternalError } from "@walletconnect/utils";
+
+import {
+  RELAYER_SDK_VERSION,
+  RELAYER_CONTEXT,
+  RELAYER_DEFAULT_LOGGER,
+  RELAYER_EVENTS,
+  RELAYER_PROVIDER_EVENTS,
+  RELAYER_RECONNECT_TIMEOUT,
+  RELAYER_SUBSCRIBER_SUFFIX,
+  RELAYER_DEFAULT_RELAY_URL,
+} from "../constants";
+import { MessageTracker } from "./messages";
+import { Publisher } from "./publisher";
+import { Subscriber } from "./subscriber";
+
+export class Relayer extends IRelayer {
+  public protocol = "wc";
+  public version = 2;
+
+  public core: ICore;
+  public logger: Logger;
+  public events = new EventEmitter();
+  public provider: IJsonRpcProvider;
+  public messages: IMessageTracker;
+  public subscriber: ISubscriber;
+  public publisher: IPublisher;
+  public name = RELAYER_CONTEXT;
+
+  private initialized = false;
+
+  private relayUrl: string;
+  private projectId: string | undefined;
+
+  constructor(opts: RelayerOptions) {
+    super(opts);
+    this.core = opts.core;
+    this.logger =
+      typeof opts.logger !== "undefined" && typeof opts.logger !== "string"
+        ? generateChildLogger(opts.logger, this.name)
+        : pino(getDefaultLoggerOptions({ level: opts.logger || RELAYER_DEFAULT_LOGGER }));
+    this.messages = new MessageTracker(this.logger, opts.core);
+    this.subscriber = new Subscriber(this, this.logger);
+    this.publisher = new Publisher(this, this.logger);
+
+    this.relayUrl = opts?.relayUrl || RELAYER_DEFAULT_RELAY_URL;
+    this.projectId = opts.projectId;
+
+    // re-assigned during init()
+    this.provider = {} as IJsonRpcProvider;
+  }
+
+  public async init() {
+    this.logger.trace(`Initialized`);
+    const auth = await this.core.crypto.signJWT(this.relayUrl);
+    try {
+      this.provider = this.createProvider(auth);
+    } catch (err) {
+      this.provider.events.emit("disconnect");
+    }
+    await Promise.all([this.messages.init(), this.provider.connect(), this.subscriber.init()]);
+    this.registerEventListeners();
+    this.initialized = true;
+  }
+
+  get context() {
+    return getLoggerContext(this.logger);
+  }
+
+  get connected() {
+    return this.provider.connection.connected;
+  }
+
+  get connecting() {
+    return this.provider.connection.connecting;
+  }
+
+  public async publish(topic: string, message: string, opts?: RelayerTypes.PublishOptions) {
+    this.isInitialized();
+    await this.publisher.publish(topic, message, opts);
+    await this.recordMessageEvent({ topic, message });
+  }
+
+  public async subscribe(topic: string, opts?: RelayerTypes.SubscribeOptions) {
+    this.isInitialized();
+    const id = await this.subscriber.subscribe(topic, opts);
+    return id;
+  }
+
+  public async unsubscribe(topic: string, opts?: RelayerTypes.UnsubscribeOptions) {
+    this.isInitialized();
+    await this.subscriber.unsubscribe(topic, opts);
+  }
+
+  public on(event: string, listener: any) {
+    this.events.on(event, listener);
+  }
+
+  public once(event: string, listener: any) {
+    this.events.once(event, listener);
+  }
+
+  public off(event: string, listener: any) {
+    this.events.off(event, listener);
+  }
+
+  public removeListener(event: string, listener: any) {
+    this.events.removeListener(event, listener);
+  }
+
+  // ---------- Private ----------------------------------------------- //
+
+  private createProvider(auth: string) {
+    return new JsonRpcProvider(
+      new WsConnection(
+        formatRelayRpcUrl({
+          sdkVersion: RELAYER_SDK_VERSION,
+          protocol: this.protocol,
+          version: this.version,
+          relayUrl: this.relayUrl,
+          projectId: this.projectId,
+          auth,
+        }),
+      ),
+    );
+  }
+
+  private async recordMessageEvent(messageEvent: RelayerTypes.MessageEvent) {
+    const { topic, message } = messageEvent;
+    await this.messages.set(topic, message);
+  }
+
+  private async shouldIgnoreMessageEvent(messageEvent: RelayerTypes.MessageEvent) {
+    const { topic, message } = messageEvent;
+    if (!(await this.subscriber.isSubscribed(topic))) return true;
+    const exists = this.messages.has(topic, message);
+    return exists;
+  }
+
+  private async onProviderPayload(payload: JsonRpcPayload) {
+    this.logger.debug(`Incoming Relay Payload`);
+    this.logger.trace({ type: "payload", direction: "incoming", payload });
+    if (isJsonRpcRequest(payload)) {
+      if (!payload.method.endsWith(RELAYER_SUBSCRIBER_SUFFIX)) return;
+      const event = (payload as JsonRpcRequest<RelayJsonRpc.SubscriptionParams>).params;
+      const { topic, message } = event.data;
+      const messageEvent = { topic, message } as RelayerTypes.MessageEvent;
+      this.logger.debug(`Emitting Relayer Payload`);
+      this.logger.trace({ type: "event", event: event.id, ...messageEvent });
+      this.events.emit(event.id, messageEvent);
+      await this.acknowledgePayload(payload);
+      await this.onMessageEvent(messageEvent);
+    }
+  }
+
+  private async onMessageEvent(messageEvent: RelayerTypes.MessageEvent) {
+    if (await this.shouldIgnoreMessageEvent(messageEvent)) return;
+    this.events.emit(RELAYER_EVENTS.message, messageEvent);
+    await this.recordMessageEvent(messageEvent);
+  }
+
+  private async acknowledgePayload(payload: JsonRpcPayload) {
+    const response = formatJsonRpcResult(payload.id, true);
+    await this.provider.connection.send(response);
+  }
+
+  private registerEventListeners() {
+    this.provider.on(RELAYER_PROVIDER_EVENTS.payload, (payload: JsonRpcPayload) =>
+      this.onProviderPayload(payload),
+    );
+    this.provider.on(RELAYER_PROVIDER_EVENTS.connect, () => {
+      this.events.emit(RELAYER_EVENTS.connect);
+    });
+    this.provider.on(RELAYER_PROVIDER_EVENTS.disconnect, () => {
+      this.events.emit(RELAYER_EVENTS.disconnect);
+      // Attempt reconnection after one second.
+      setTimeout(() => {
+        this.provider.connect();
+      }, toMiliseconds(RELAYER_RECONNECT_TIMEOUT));
+    });
+    this.provider.on(RELAYER_PROVIDER_EVENTS.error, (err: unknown) =>
+      this.events.emit(RELAYER_EVENTS.error, err),
+    );
+  }
+
+  private isInitialized() {
+    if (!this.initialized) {
+      const { message } = getInternalError("NOT_INITIALIZED", this.name);
+      throw new Error(message);
+    }
+  }
+}
