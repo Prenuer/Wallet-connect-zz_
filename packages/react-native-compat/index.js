eeaf953872f07971d47b11a0c29cdb8ad0b96911
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 16:03:47 +0100

    Initialize chore/utils-side-effects

---
+++ packages/react-native-compat/index.js
@@ -0,0 +1,28 @@
+// Polyfill TextEncode / TextDecode
+import "fast-text-encoding";
+
+// Polyfill crypto.getRandomvalues
+import "react-native-get-random-values";
+
+// Polyfill Buffer
+if (typeof Buffer === "undefined") {
+  global.Buffer = require("buffer").Buffer;
+}
+
+if (typeof global?.Linking === "undefined") {
+  try {
+    global.Linking = require("react-native").Linking;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error("react-native-compat: react-native.Linking is not available");
+  }
+}
+
+if (typeof global?.Platform === "undefined") {
+  try {
+    global.Platform = require("react-native").Platform;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error("react-native-compat: react-native.Platform is not available");
+  }
+}
