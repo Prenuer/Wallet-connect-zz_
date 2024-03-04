ef933754a537b82b2d5dcf5b87e2d53dcf8f3379
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 16:02:19 +0100

    Initialize chore/starknet-provider

---
+++ packages/utils/test/validators.spec.ts
@@ -0,0 +1,330 @@
+import { expect, describe, it } from "vitest";
+import {
+  TEST_CHAINS,
+  TEST_ETHEREUM_CHAIN_A,
+  TEST_ETHEREUM_NAMESPACE,
+  TEST_EVENTS,
+  TEST_METHODS,
+  TEST_SESSION,
+} from "./shared/values";
+
+import { isConformingNamespaces, isSessionCompatible } from "../src";
+
+describe("Validators", () => {
+  it("isSessionCompatible", () => {
+    // exact
+    expect(
+      isSessionCompatible(TEST_SESSION, {
+        requiredNamespaces: {
+          [TEST_ETHEREUM_NAMESPACE]: {
+            chains: TEST_CHAINS,
+            methods: TEST_METHODS,
+            events: TEST_EVENTS,
+          },
+        },
+      }),
+    ).to.be.true;
+    // chains
+    expect(
+      isSessionCompatible(TEST_SESSION, {
+        requiredNamespaces: {
+          [TEST_ETHEREUM_NAMESPACE]: {
+            chains: [TEST_ETHEREUM_CHAIN_A],
+            methods: TEST_METHODS,
+            events: TEST_EVENTS,
+          },
+        },
+      }),
+    ).to.be.true;
+    expect(
+      isSessionCompatible(TEST_SESSION, {
+        requiredNamespaces: {
+          [TEST_ETHEREUM_NAMESPACE]: {
+            chains: [...TEST_CHAINS, "eip155:100"],
+            methods: TEST_METHODS,
+            events: TEST_EVENTS,
+          },
+        },
+      }),
+    ).to.be.false;
+    // methods
+    expect(
+      isSessionCompatible(TEST_SESSION, {
+        requiredNamespaces: {
+          [TEST_ETHEREUM_NAMESPACE]: {
+            chains: TEST_CHAINS,
+            methods: ["personal_sign"],
+            events: TEST_EVENTS,
+          },
+        },
+      }),
+    ).to.be.true;
+    expect(
+      isSessionCompatible(TEST_SESSION, {
+        requiredNamespaces: {
+          [TEST_ETHEREUM_NAMESPACE]: {
+            chains: TEST_CHAINS,
+            methods: [...TEST_METHODS, "blockchain_signMessage"],
+            events: TEST_EVENTS,
+          },
+        },
+      }),
+    ).to.be.false;
+    // events
+    expect(
+      isSessionCompatible(TEST_SESSION, {
+        requiredNamespaces: {
+          [TEST_ETHEREUM_NAMESPACE]: {
+            chains: TEST_CHAINS,
+            methods: TEST_METHODS,
+            events: ["accountsChanged"],
+          },
+        },
+      }),
+    ).to.be.true;
+    expect(
+      isSessionCompatible(TEST_SESSION, {
+        requiredNamespaces: {
+          [TEST_ETHEREUM_NAMESPACE]: {
+            chains: TEST_CHAINS,
+            methods: TEST_METHODS,
+            events: [...TEST_EVENTS, `colorChanged`],
+          },
+        },
+      }),
+    ).to.be.false;
+    // wrong namespace
+    expect(
+      isSessionCompatible(TEST_SESSION, {
+        requiredNamespaces: {
+          cosmos: {
+            chains: TEST_CHAINS,
+            methods: TEST_METHODS,
+            events: TEST_EVENTS,
+          },
+        },
+      }),
+    ).to.be.false;
+  });
+  it("should validate namespaces (configuration 1)", () => {
+    const required = {
+      eip155: {
+        chains: ["eip155:1", "eip155:2", "eip155:3"],
+        events: [],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+      "eip155:4": {
+        events: [],
+        methods: ["eth_accounts"],
+      },
+      solana: {
+        chains: ["solana:1", "solana:2", "solana:3"],
+        events: [],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+    };
+
+    const approved = {
+      eip155: {
+        accounts: [
+          "eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+          "eip155:2:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+          "eip155:3:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+        ],
+        events: [],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+      "eip155:4": {
+        events: [],
+        methods: ["eth_accounts"],
+        accounts: ["eip155:4:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+      },
+      "eip155:5": {
+        events: [],
+        methods: ["eth_sendTransaction"],
+        accounts: ["eip155:5:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+      },
+      solana: {
+        accounts: [
+          "solana:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+          "solana:2:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+          "solana:3:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+        ],
+        events: [],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+    };
+    const err = isConformingNamespaces(required, approved, "validators");
+    expect(err).to.be.null;
+  });
+  it("should validate namespaces (configuration 2)", () => {
+    const required = {
+      "eip155:1": {
+        events: [],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+      "eip155:2": {
+        events: [],
+        methods: ["eth_accounts"],
+      },
+      "solana:1": {
+        events: [],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+    };
+
+    const approved = {
+      eip155: {
+        accounts: [
+          "eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+          "eip155:2:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+          "eip155:3:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+        ],
+        events: [],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+      solana: {
+        accounts: ["solana:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+        events: [],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+    };
+    const err = isConformingNamespaces(required, approved, "validators");
+    expect(err).to.be.null;
+  });
+  it("should validate namespaces (configuration 3)", () => {
+    const required = {
+      eip155: {
+        chains: ["eip155:1"],
+        events: [],
+        methods: ["eth_accounts"],
+      },
+    };
+
+    const approved = {
+      eip155: {
+        accounts: [
+          "eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+          "eip155:2:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092",
+        ],
+        events: ["chainChanged"],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+    };
+    const err = isConformingNamespaces(required, approved, "validators");
+    expect(err).to.be.null;
+  });
+
+  it("should validate namespaces (configuration 4)", () => {
+    const required = {
+      eip155: {
+        chains: ["eip155:1", "eip155:2"],
+        events: [],
+        methods: ["eth_accounts"],
+      },
+    };
+
+    const approved = {
+      "eip155:1": {
+        accounts: ["eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+        events: ["chainChanged"],
+        methods: ["eth_accounts"],
+      },
+      "eip155:2": {
+        accounts: ["eip155:2:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+        events: ["chainChanged"],
+        methods: ["eth_accounts"],
+      },
+    };
+    const err = isConformingNamespaces(required, approved, "validators");
+    expect(err).to.be.null;
+  });
+
+  it("should throw on invalid accounts", () => {
+    const required = {
+      eip155: {
+        chains: ["eip155:1"],
+        events: [],
+        methods: ["eth_accounts"],
+      },
+    };
+
+    const approved = {
+      eip155: {
+        accounts: ["eip155:2:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+        events: ["chainChanged"],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+    };
+    const error = isConformingNamespaces(required, approved, "validators");
+    expect(error).to.not.be.null;
+    expect(error).to.throw;
+  });
+  it("should throw on invalid namespace", () => {
+    const required = {
+      eip155: {
+        chains: ["eip155:1"],
+        events: [],
+        methods: ["eth_accounts"],
+      },
+    };
+
+    const approved = {
+      solana: {
+        accounts: ["solana:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+        events: ["chainChanged"],
+        methods: ["eth_accounts", "personal_sign"],
+      },
+    };
+    const error = isConformingNamespaces(required, approved, "validators");
+    expect(error).to.not.be.null;
+    expect(error).to.throw;
+  });
+
+  it("should throw on invalid methods", () => {
+    const required = {
+      eip155: {
+        chains: ["eip155:1"],
+        events: [],
+        methods: ["eth_accounts"],
+      },
+    };
+
+    const approved = {
+      eip155: {
+        accounts: ["eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+        events: ["chainChanged"],
+        methods: ["personal_sign"],
+      },
+    };
+    const error = isConformingNamespaces(required, approved, "validators");
+    expect(error).to.not.be.null;
+    expect(error).to.throw;
+  });
+
+  it("should throw on CAIP2 namespace not including that CAIP2 in accouns", () => {
+    const required = {
+      eip155: {
+        chains: ["eip155:1", "eip155:2"],
+        events: [],
+        methods: ["eth_accounts"],
+      },
+    };
+
+    const approved = {
+      "eip155:1": {
+        accounts: ["eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+        events: ["chainChanged"],
+        methods: ["eth_accounts"],
+      },
+      "eip155:2": {
+        accounts: ["eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092"],
+        events: ["chainChanged"],
+        methods: ["eth_accounts"],
+      },
+    };
+    const error = isConformingNamespaces(required, approved, "validators");
+    expect(error).to.not.be.null;
+    expect(error).to.throw;
+  });
+});
---
+++ providers/universal-provider/src/providers/index.ts
@@ -0,0 +1,5 @@
+export * as CardanoProvider from "./cardano";
+export * as CosmosProvider from "./cosmos";
+export * as Eip155Provider from "./eip155";
+export * as SolanaProvider from "./solana";
+export * as StarknetProvider from "./starknet";
