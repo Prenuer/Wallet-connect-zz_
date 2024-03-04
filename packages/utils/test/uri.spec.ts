9dc6cc60ecdcb65f2e9e2f16945f908fc132eb2e
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 16:01:51 +0100

    Initialize chore/set-loglevel-for-tests

---
+++ packages/utils/test/uri.spec.ts
@@ -0,0 +1,32 @@
+import { EngineTypes } from "@walletconnect/types";
+import { expect, describe, it } from "vitest";
+import { formatUri, generateRandomBytes32, hashKey, parseUri } from "../src";
+import { TEST_PAIRING_TOPIC, TEST_RELAY_OPTIONS, TEST_SYM_KEY } from "./shared";
+
+const symKey = generateRandomBytes32();
+const topic = hashKey(symKey);
+
+const TEST_URI_PARAMS: EngineTypes.UriParameters = {
+  protocol: "wc",
+  version: 2,
+  topic,
+  symKey,
+  relay: TEST_RELAY_OPTIONS,
+};
+
+const TEST_URI_STRING = `${TEST_URI_PARAMS.protocol}:${TEST_URI_PARAMS.topic}@${TEST_URI_PARAMS.version}?relay-protocol=${TEST_RELAY_OPTIONS.protocol}&symKey=${TEST_URI_PARAMS.symKey}`;
+
+describe("URI", () => {
+  it("formatUri", () => {
+    const uri = formatUri(TEST_URI_PARAMS);
+    expect(uri).to.eql(TEST_URI_STRING);
+  });
+  it("parseUri", () => {
+    const uriParams = parseUri(TEST_URI_STRING);
+    expect(uriParams.version).to.eql(TEST_URI_PARAMS.version);
+    expect(uriParams.topic).to.eql(TEST_URI_PARAMS.topic);
+    expect(uriParams.symKey).to.eql(TEST_URI_PARAMS.symKey);
+    expect(uriParams.relay.data).to.eql(TEST_URI_PARAMS.relay.data);
+    expect(uriParams.relay.protocol).to.eql(TEST_URI_PARAMS.relay.protocol);
+  });
+});
