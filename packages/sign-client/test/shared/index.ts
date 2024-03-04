6b2eb84b03d272a76dd1914708f892e027de2657
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 15:59:19 +0100

    Initialize feat/solana-universal-provider

---
+++ packages/sign-client/test/shared/index.ts
@@ -0,0 +1,7 @@
+export * from "./connect";
+export * from "./init";
+export * from "./mock";
+export * from "./values";
+export * from "./helpers";
+export * from "./metrics";
+export * from "./ws";
---
+++ packages/types/src/core/messages.ts
@@ -0,0 +1,25 @@
+import { Logger } from "pino";
+
+import { ICore } from "./core";
+
+export type MessageRecord = Record<string, string>;
+
+export abstract class IMessageTracker {
+  public abstract messages: Map<string, MessageRecord>;
+
+  public abstract name: string;
+
+  public abstract readonly context: string;
+
+  constructor(public logger: Logger, public core: ICore) {}
+
+  public abstract init(): Promise<void>;
+
+  public abstract set(topic: string, message: string): Promise<string>;
+
+  public abstract get(topic: string): MessageRecord;
+
+  public abstract has(topic: string, message: string): boolean;
+
+  public abstract del(topic: string): Promise<void>;
+}
