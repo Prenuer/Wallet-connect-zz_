efd82e02e72cf88a1e9147975359830bb4179a82
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 15:57:49 +0100

    Initialize ilja/2.0.0-rc.1

---
+++ packages/core/src/constants/relayer.ts
@@ -0,0 +1,35 @@
+import { ONE_SECOND } from "@walletconnect/time";
+
+export const RELAYER_DEFAULT_PROTOCOL = "iridium";
+
+export const RELAYER_DEFAULT_LOGGER = "error";
+
+export const RELAYER_DEFAULT_RELAY_URL = "wss://relay.walletconnect.com";
+
+export const RELAYER_CONTEXT = "relayer";
+
+export const RELAYER_EVENTS = {
+  message: "relayer_message",
+  connect: "relayer_connect",
+  disconnect: "relayer_disconnect",
+  error: "relayer_error",
+};
+
+export const RELAYER_SUBSCRIBER_SUFFIX = "_subscription";
+
+export const RELAYER_PROVIDER_EVENTS = {
+  payload: "payload",
+  connect: "connect",
+  disconnect: "disconnect",
+  error: "error",
+};
+
+export const RELAYER_RECONNECT_TIMEOUT = ONE_SECOND;
+
+export const RELAYER_STORAGE_OPTIONS = {
+  database: ":memory:",
+};
+
+// FIXME: Always manually change this after each release (hardcoded from package.json)
+// ... this will be temporarily hardcoded until we refactor our build chain!
+export const RELAYER_SDK_VERSION = "2.0.0-rc.1";
