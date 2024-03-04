02df072ae082c0ccc81a692983089586aa5ad718
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 15:58:33 +0100

    Initialize debug/canary

---
+++ packages/sign-client/test/shared/init.ts
@@ -0,0 +1,17 @@
+import { SignClientTypes } from "@walletconnect/types";
+import SignClient from "../../src";
+import { TEST_SIGN_CLIENT_OPTIONS_A, TEST_SIGN_CLIENT_OPTIONS_B } from "./values";
+
+export interface Clients {
+  A: SignClient;
+  B: SignClient;
+}
+
+export async function initTwoClients(clientOpts: SignClientTypes.Options = {}) {
+  //TEST_SIGN_CLIENT_OPTIONS_A.relayUrl = 'ws://localhost:9080';
+  //TEST_SIGN_CLIENT_OPTIONS_B.relayUrl = 'ws://localhost:8080';
+  console.log(TEST_SIGN_CLIENT_OPTIONS_A, TEST_SIGN_CLIENT_OPTIONS_B);
+  const A = await SignClient.init({ ...TEST_SIGN_CLIENT_OPTIONS_A, ...clientOpts });
+  const B = await SignClient.init({ ...TEST_SIGN_CLIENT_OPTIONS_B, ...clientOpts });
+  return { A, B };
+}
