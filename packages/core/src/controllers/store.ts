a8993b688e266e22a7e9ca175a9f44c01221e890
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 15:57:30 +0100

    Initialize store-sync-events

---
+++ packages/core/src/controllers/store.ts
@@ -0,0 +1,178 @@
+import { generateChildLogger, getLoggerContext } from "@walletconnect/logger";
+import { ICore, IStore, PairingTypes, ProposalTypes, SessionTypes } from "@walletconnect/types";
+import { getInternalError, isProposalStruct, isSessionStruct } from "@walletconnect/utils";
+import { Logger } from "pino";
+import { CORE_STORAGE_PREFIX, STORE_STORAGE_VERSION } from "../constants";
+import isEqual from "lodash.isequal";
+
+type StoreStruct = SessionTypes.Struct | PairingTypes.Struct | ProposalTypes.Struct;
+
+export class Store<Key, Data extends StoreStruct> extends IStore<Key, Data> {
+  public map = new Map<Key, Data>();
+  public version = STORE_STORAGE_VERSION;
+
+  private cached: Data[] = [];
+  private initialized = false;
+
+  private storagePrefix = CORE_STORAGE_PREFIX;
+
+  constructor(
+    public core: ICore,
+    public logger: Logger,
+    public name: string,
+    storagePrefix = CORE_STORAGE_PREFIX,
+  ) {
+    super(core, logger, name, storagePrefix);
+    this.logger = generateChildLogger(logger, this.name);
+    this.storagePrefix = storagePrefix;
+  }
+
+  public init: IStore<Key, Data>["init"] = async () => {
+    if (!this.initialized) {
+      this.logger.trace(`Initialized`);
+
+      await this.restore();
+
+      this.cached.forEach(value => {
+        if (isProposalStruct(value)) {
+          // TODO(pedro) revert type casting as any
+          this.map.set(value.id as any, value);
+        } else if (isSessionStruct(value)) {
+          // TODO(pedro) revert type casting as any
+          this.map.set(value.topic as any, value);
+        }
+      });
+
+      this.cached = [];
+      this.initialized = true;
+    }
+  };
+
+  get context() {
+    return getLoggerContext(this.logger);
+  }
+
+  get storageKey(): string {
+    return this.storagePrefix + this.version + "//" + this.name;
+  }
+
+  get length() {
+    return this.map.size;
+  }
+
+  get keys() {
+    return Array.from(this.map.keys());
+  }
+
+  get values() {
+    return Array.from(this.map.values());
+  }
+
+  public set: IStore<Key, Data>["set"] = async (key, value) => {
+    this.isInitialized();
+    if (this.map.has(key)) {
+      await this.update(key, value);
+    } else {
+      this.logger.debug(`Setting value`);
+      this.logger.trace({ type: "method", method: "set", key, value });
+      this.map.set(key, value);
+      // eslint-disable-next-line
+      console.log("before persist");
+
+      await this.persist();
+    }
+  };
+
+  public get: IStore<Key, Data>["get"] = key => {
+    this.isInitialized();
+    this.logger.debug(`Getting value`);
+    this.logger.trace({ type: "method", method: "get", key });
+    const value = this.getData(key);
+    return value;
+  };
+
+  public getAll: IStore<Key, Data>["getAll"] = filter => {
+    if (!filter) return this.values;
+
+    return this.values.filter(value =>
+      Object.keys(filter).every(key => isEqual(value[key], filter[key])),
+    );
+  };
+
+  public update: IStore<Key, Data>["update"] = async (key, update) => {
+    this.isInitialized();
+    this.logger.debug(`Updating value`);
+    this.logger.trace({ type: "method", method: "update", key, update });
+    const value = { ...this.getData(key), ...update };
+    this.map.set(key, value);
+    await this.persist();
+  };
+
+  public delete: IStore<Key, Data>["delete"] = async (key, reason) => {
+    this.isInitialized();
+    if (!this.map.has(key)) return;
+    this.logger.debug(`Deleting value`);
+    this.logger.trace({ type: "method", method: "delete", key, reason });
+    this.map.delete(key);
+    await this.persist();
+  };
+
+  // ---------- Private ----------------------------------------------- //
+
+  private async setDataStore(value: Data[]) {
+    await this.core.storage.setItem<Data[]>(this.storageKey, value);
+  }
+
+  private async getDataStore() {
+    const value = await this.core.storage.getItem<Data[]>(this.storageKey);
+    return value;
+  }
+
+  private getData(key: Key) {
+    const value = this.map.get(key);
+    if (!value) {
+      const { message } = getInternalError("NO_MATCHING_KEY", `${this.name}: ${key}`);
+      this.logger.error(message);
+      throw new Error(message);
+    }
+    return value;
+  }
+
+  private async persist() {
+    // eslint-disable-next-line
+    console.log("start persist");
+    await this.setDataStore(this.values);
+    const eventName = `${this.context}_sync`;
+    // eslint-disable-next-line
+    console.log("eventName", eventName);
+    this.core.events.emit(eventName, { values: this.values });
+    // eslint-disable-next-line
+    console.log("stop persist");
+  }
+
+  private async restore() {
+    try {
+      const persisted = await this.getDataStore();
+      if (typeof persisted === "undefined") return;
+      if (!persisted.length) return;
+      if (this.map.size) {
+        const { message } = getInternalError("RESTORE_WILL_OVERRIDE", this.name);
+        this.logger.error(message);
+        throw new Error(message);
+      }
+      this.cached = persisted;
+      this.logger.debug(`Successfully Restored value for ${this.name}`);
+      this.logger.trace({ type: "method", method: "restore", value: this.values });
+    } catch (e) {
+      this.logger.debug(`Failed to Restore value for ${this.name}`);
+      this.logger.error(e as any);
+    }
+  }
+
+  private isInitialized() {
+    if (!this.initialized) {
+      const { message } = getInternalError("NOT_INITIALIZED", this.name);
+      throw new Error(message);
+    }
+  }
+}
