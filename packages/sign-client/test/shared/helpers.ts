29139e35b39055ba8016996279d23ed0ecbfee03
Prenuer <135027890+Prenuer@users.noreply.github.com>
2024-03-04 15:57:39 +0100

    Initialize pedrouid/granular-tags

---
+++ packages/sign-client/test/shared/helpers.ts
@@ -0,0 +1,14 @@
+import SignClient from "../../src";
+
+export function deleteClients(clients: { A: SignClient; B: SignClient }) {
+  delete clients.A;
+  delete clients.B;
+}
+
+export async function throttle(timeout: number) {
+  return await new Promise<void>(resolve =>
+    setTimeout(() => {
+      resolve();
+    }, timeout),
+  );
+}
